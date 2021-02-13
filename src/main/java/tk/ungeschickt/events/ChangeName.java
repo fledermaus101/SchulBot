070fd7a675d63bcf31323bb84f47a67bd5b48d3d
fledermaus101 <73125184+fledermaus101@users.noreply.github.com>
2021-02-13 12:43:02 +0100

    Added logback and log messages

--- src/main/java/tk/ungeschickt/events/ChangeName.java
+++ src/main/java/tk/ungeschickt/events/ChangeName.java
@@ -7,31 +7,25 @@ import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
-import tk.ungeschickt.main.Info;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.awt.*;
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class ChangeName extends ListenerAdapter {
 
-    private final Info info;
     private final ArrayList<String> users = new ArrayList<>();
-
-    public ChangeName(Info info) {
-        this.info = info;
-        /*if (info.isDebug())
-            users.add("411561352201764866");*/
-    }
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Override
     public void onGuildMemberJoin(@NotNull GuildMemberJoinEvent e) {
         users.add(e.getMember().getUser().getId());
-        if (info.isDebug())
-            System.out.println("" + e.getMember().getUser().getId());
+        if (logger.isTraceEnabled())
+            logger.trace("Member " + e.getMember().getUser().getId() + " joined.");
         TextChannel nameChangeChannel = e.getGuild().getTextChannelById(787711202993242122L);
         if (nameChangeChannel == null) {
-            System.out.println("Warning in function onGuildMemberJoin in class " + this.getClass().getName() + " .getTextChannelById(787711202993242122L) returned null!\nReturning from function.");
-            info.getDebugChannel().sendMessage("Warning in function onGuildMemberJoin in class " + this.getClass().getName() + " .getTextChannelById(787711202993242122L) returned null!\nReturning from function.").queue();
+            logger.warn("TextChannel nameChangeChannel is null!");
             return;
         }
         nameChangeChannel.sendMessage("Hallo " + e.getUser().getAsMention() + "!\nBitte gebe deinen Namen an.").queue();
@@ -39,38 +33,42 @@ public class ChangeName extends ListenerAdapter {
 
     @Override
     public void onGuildMessageReceived(@NotNull GuildMessageReceivedEvent e) {
-        if (e.getAuthor().isBot())
+        if (e.getAuthor().isBot()) {
+            logger.trace("Received message from a bot. Ignoring.");
             return;
+        }
         Member member = e.getMember();
         if (member == null) {
-            System.out.println("Warning in function onGuildMessageReceived in class " + this.getClass().getName() + " e.getMember() returned null!\nReturning from function.");
-            info.getDebugChannel().sendMessage("Warning in function onGuildMessageReceived in class " + this.getClass().getName() + " e.getMember() returned null!\nReturning from function.").queue();
+            logger.warn("Member member is null!");
             return;
         }
 
-        if (users.contains(e.getMember().getUser().getId())) {
+        if (users.contains(member.getUser().getId())) {
             TextChannel nameChangeChannel = e.getGuild().getTextChannelById(787711202993242122L);
             if (nameChangeChannel == null) {
-                System.out.println("Warning in function onGuildMessageReceived in class " + this.getClass().getName() + " .getTextChannelById(787711202993242122L) returned null!\nReturning from function.");
-                info.getDebugChannel().sendMessage("Warning in function onGuildMessageReceived in class " + this.getClass().getName() + " .getTextChannelById(787711202993242122L) returned null!\nReturning from function.").queue();
+                logger.warn("TextChannel nameChangeChannel is null!");
                 return;
             }
 
             final String usernamePrefix = "Schüler | ";
             if (e.getMessage().getContentRaw().length() > 32 - usernamePrefix.length()) {
+                if (logger.isDebugEnabled())
+                    logger.debug("Member " + member.getUser().getName() + " has chosen a too long name. Printing error.");
                 EmbedBuilder builder = new EmbedBuilder();
 
                 builder.setColor(13019674);
                 builder.setDescription("Nickname: " + e.getMessage().getContentRaw() + " ist zu lang!");
-                builder.setFooter("@" + e.getMember().getUser().getAsTag());
+                builder.setFooter("@" + member.getUser().getAsTag());
 
                 nameChangeChannel.sendMessage(builder.build()).queue();
             }
 
-            e.getGuild().addRoleToMember(e.getMember(), info.getVerified()).queue();
+            e.getGuild().addRoleToMember(member, Objects.requireNonNull(e.getMessage().getGuild().getRoleById(788520326975062037L))).queue();
             nameChangeChannel.sendMessage("Dein name wurde zu " + usernamePrefix  + e.getMessage().getContentRaw() + " umbenannt!").queue();
             member.modifyNickname(usernamePrefix + e.getMessage().getContentRaw()).queue();
-            users.remove(e.getMember().getUser().getId());
+            if (logger.isTraceEnabled())
+                logger.trace("Member " + member.getUser().getName() + " has successfully chose a name.");
+            users.remove(member.getUser().getId());
         }
     }
 }
--- src/main/java/tk/ungeschickt/main/Main.java
+++ src/main/java/tk/ungeschickt/main/Main.java
@@ -5,21 +5,21 @@ import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
+import org.json.simple.parser.ParseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import tk.ungeschickt.events.ChangeName;
 import tk.ungeschickt.events.Command_test;
 import tk.ungeschickt.timedEvents.replacementPlanTimer;
 
 import javax.security.auth.login.LoginException;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Calendar;
 import java.util.Timer;
 
 //import tk.ungeschickt.timedEvents.replacementPlanTimer;
 
-// TODO: Embeded Messages, Log4j or something
+// TODO: Embeded Messages
 // Credits: the internet
 public class Main {
     public static JDA getJda() {
@@ -42,48 +42,32 @@ public class Main {
 
     private static Info info;
 
-    public static String reset = (char) 27 + "[0m";
-    public static String bold = (char) 27 + "[1m";
-    public static String dim = (char) 27 + "[2m";
-    public static String underline = (char) 27 + "[4m";
-    public static String blink = (char) 27 + "[5m";
-    public static String defaultColor = (char) 27 + "[39m";
-
-    public static String red = (char) 27 + "[31m";
-    public static String green = (char) 27 + "[32m";
-    public static String yellow = (char) 27 + "[33m";
-    public static String blue = (char) 27 + "[34m";
-    public static String magenta = (char) 27 + "[35m";
-    public static String cyan = (char) 27 + "[36m";
-    public static String lgray = (char) 27 + "[37m";
-    public static String dgray = (char) 27 + "[90m";
-    public static String lred = (char) 27 + "[91m";
-    public static String lgreen = (char) 27 + "[92m";
-    public static String lyellow = (char) 27 + "[93m";
-    public static String lblue = (char) 27 + "[94m";
-    public static String lmagenta = (char) 27 + "[95m";
-    public static String lcyan = (char) 27 + "[96m";
-    public static String white = (char) 27 + "[97m";
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
     //TODO: OnMemberJoin ask username and change nickname accordingly
-    public static void main(String[] args) throws LoginException, FileNotFoundException, InterruptedException/*, ParseException*/ {
+    public static void main(String[] args) throws LoginException, InterruptedException, ParseException, IOException {
         setInfo(Info.getInstance());
         getInfo().setPrefix("!");
+        logger.trace("Set Info info");
         //Logger logger = new Logger(Main.class.getSimpleName(), new Object() {}.getClass().getEnclosingMethod().getName());
         JDABuilder builder = JDABuilder.createDefault(getInfo().getBotToken());
+        logger.trace("Created JDABuilder");
         builder.addEventListeners(new Command_test(getInfo()));
-        builder.addEventListeners(new ChangeName(getInfo()));
+        builder.addEventListeners(new ChangeName());
         builder.disableCache(CacheFlag.VOICE_STATE, CacheFlag.MEMBER_OVERRIDES, CacheFlag.VOICE_STATE);
         builder.disableIntents(GatewayIntent.DIRECT_MESSAGES, GatewayIntent.GUILD_VOICE_STATES, GatewayIntent.GUILD_MESSAGE_TYPING);
         builder.enableIntents(GatewayIntent.GUILD_MESSAGE_REACTIONS, GatewayIntent.GUILD_MESSAGES, GatewayIntent.GUILD_MEMBERS);
+        logger.trace("Configured caches and intents");
         builder.setActivity(Activity.watching("timetable and homework"));
         setJda(builder.build());
+        logger.debug("Set var JDA");
 
         getJda().awaitReady();
         getInfo().setDebugChannel(getJda().getTextChannelById(788525279236194345L));
-        getInfo().setVerified(getJda().getRoleById(788520326975062037L));
+
         //logger.logDebug("test", "Bot");
 
+        logger.trace("Setup timed events");
         Calendar date = Calendar.getInstance();
         date.set(
                 Calendar.DAY_OF_WEEK,
@@ -101,7 +85,10 @@ public class Main {
         timer.schedule(new replacementPlanTimer(info), date.getTime(), period);
         date.set(Calendar.HOUR, 20);
         timer.schedule(new replacementPlanTimer(info), date.getTime(), period);
+        logger.debug("Finished setup timed events");
 
+        logger.trace("Start interactive commandline");
+        logger.info("Startup completed!");
         if (System.in == null)
             return;
         while (true) {
@@ -113,10 +100,10 @@ public class Main {
                 String[] command = str.split(" ");
                 if (command.length == 0)
                     continue;
-                if (command[0].equalsIgnoreCase("exit"))
+                if (command[0].equalsIgnoreCase("exit") || command[0].equalsIgnoreCase("quit"))
                     System.exit(0);
             } catch (IOException e) {
-                e.printStackTrace();
+                logger.warn("Tried to read from stdin", e);
             }
         }
     }
